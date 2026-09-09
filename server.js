5e2b14b7159e0d1fdf84855309d5f8aad721c14e
Nagis <jeulis8@gmail.com>
2026-09-09 03:31:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const { Pool } = require("pg");
 
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
-
+app.use(express.static(path.join(__dirname, ".")));
 // --- Connexion PostgreSQL ---
 // Coolify fournit l'URL de connexion via la variable DATABASE_URL.
 const pool = new Pool({
